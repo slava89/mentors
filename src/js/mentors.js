d96741fc2e1b82c43d24c79e458e290ece167b8c
Slava Krivtsov <slava.kr.89@gmail.com>
2015-09-17 23:28:45 +0300

    add json file

--- src/js/mentors.js
+++ src/js/mentors.js
@@ -5,7 +5,7 @@
         function($http) {
             var mentor = this;
             mentor.mentorInfo = [];
-            $http.get('../src/json/mentors.json').success(function(data) {
+            $http.get('/json/mentors.json').success(function(data) {
 
                 mentor.mentorInfo = data;
             });
